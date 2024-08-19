b353134ec60e76a6ca243f39205a53bf60028127
WillarDraco <a1863235@adelaide.edu.au>
2024-08-19 11:47:08 +0930

    Karatsuba

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 #include <string>
+
+std::vector<int> convertToVector(int I1, int B) {
+    std::vector<int> v1;
+    int t1 = I1;
+
+    while (t1 != 0) {
+        v1.push_back(t1 % B);
+        t1 = t1 / B;
+    }
+    return v1;
+}
+
+int vectorToInt(const std::vector<int>& I1, int B) {
+    int num = 0;
+    for (int i = I1.size() - 1; i >= 0; i--) {
+        num = num * B + I1[i];
+    }
+    return num;
+}
 
 std::string school(int I1, int I2, int B) {
     std::vector<int> v1;
@@ -53,6 +72,45 @@ std::string school(int I1, int I2, int B) {
     return num;
 }
 
-int main(void) {
-    school(101, 5, 10);
+long long karatsuba(int I1, int I2, int B) {
+    if (I1 < B || I2 < B) {
+        return (long long)I1 * I2;
+    }
+
+    std::vector<int> s1 = convertToVector(I1, B);
+    std::vector<int> s2 = convertToVector(I2, B);
+
+    int maxLength = std::max(s1.size(), s2.size());
+    int midP = maxLength / 2;
+
+    std::vector<int> ls1(s1.begin(), s1.begin() + midP);
+    std::vector<int> hs1(s1.begin() + midP, s1.end());
+    std::vector<int> ls2(s2.begin(), s2.begin() + midP);
+    std::vector<int> hs2(s2.begin() + midP, s2.end());
+
+    int low1 = vectorToInt(ls1, B);
+    int high1 = vectorToInt(hs1, B);
+    int low2 = vectorToInt(ls2, B);
+    int high2 = vectorToInt(hs2, B);
+
+    long long z0 = karatsuba(low1, low2, B);
+    long long z1 = karatsuba(low1 + high1, low2 + high2, B);
+    long long z2 = karatsuba(high1, high2, B);
+
+    return (z2 * pow(B, 2 * midP) + ((z1 - z2 - z0) * pow(B, midP)) + z0);
+}
+
+int main() {
+    int I1;
+    int I2;
+    int B;
+
+    std::cin >> I1 >> I2 >> B;
+
+    std::string t1 = school(I1, I2, B);
+    
+    long long t2 = karatsuba(I1, I2, B);
+    std::cout << t1 << " " << t2 << " " << 0 << std::endl;
+
+    return 0;
 }
